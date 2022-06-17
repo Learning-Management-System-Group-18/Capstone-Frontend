a5a6e3a018d89b3f74301c05f3610f607b098137
Learning-Management-System-Group-18 <yusufmaolana001@ummi.ac.id>
2022-06-17 11:34:17 +0700

    update button

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,35 +1,64 @@
 import React from 'react';
 import './style.css';
 import { AiOutlineArrowLeft, AiOutlinePlus } from 'react-icons/ai';
+import { RiPencilFill } from 'react-icons/ri';
+import { FaTrash } from 'react-icons/fa';
 
 const index = ({ type }) => {
-  // let type = 'add';
   return (
     <div>
-      {type == 'save' ? (
+      {type == 'btn-save' ? (
         <button type="button" class="btn btn-save">
           Save
         </button>
       ) : (
         ''
       )}
-      {type == 'next' ? (
+      {type == 'btn-next' ? (
         <button type="button" class="btn btn-save">
           Next Step
         </button>
       ) : (
         ''
       )}
-      {type == 'back' ? (
+      {type == 'btn-back' ? (
         <button type="button" class="btn btn-back">
           <AiOutlineArrowLeft className="icon-back" /> Back
         </button>
       ) : (
         ''
       )}
-      {type == 'add' ? (
+      {type == 'btn-add' ? (
         <button type="button" class="btn btn-add">
           <AiOutlinePlus className="icon-add" /> Create Category
+        </button>
+      ) : (
+        ''
+      )}
+      {type == 'btn-popupsave' ? (
+        <button type="button" class="btn btn-popupsave">
+          Save
+        </button>
+      ) : (
+        ''
+      )}
+      {type == 'btn-popupcancel' ? (
+        <button type="button" class="btn btn-popupcancel">
+          Cancel
+        </button>
+      ) : (
+        ''
+      )}
+      {type == 'btn-edit' ? (
+        <button type="button" class="btn btn-edit">
+          <RiPencilFill className="icon-edit" /> Edit
+        </button>
+      ) : (
+        ''
+      )}
+      {type == 'btn-delete' ? (
+        <button type="button" class="btn btn-delete">
+          <FaTrash className="icon-delete" /> Delete
         </button>
       ) : (
         ''
