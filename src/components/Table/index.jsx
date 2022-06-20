6931f569ea5816b977ab5e60e6cb1b1b7a2e55bc
mumtazdedi <mumtazdedi@gmail.com>
2022-06-21 02:22:40 +0700

    fixing some ui in table component

--- src/components/Table/index.jsx
+++ src/components/Table/index.jsx
@@ -2,17 +2,18 @@ import React from 'react';
 import './style.css';
 import { Accordion } from 'react-bootstrap';
 import { useLocation, Link } from 'react-router-dom';
+import { Button } from '../';
 
 const Index = ({ tHead, data, tableTitle }) => {
 
     const location = useLocation();
 
     return (
-        <div className='rounded p-2 bg-light'>
+        <div className='rounded p-3 bg-light'>
             <div className='mb-5'>
-                <h3>{tableTitle}</h3>
+                <h3 className='heading_4 secondary_2'>{tableTitle}</h3>
             </div>
-            <div className='px-2'>
+            <div className='px-4'>
                 {
                     location.pathname === '/dashboard' ? (
                         <table className='table table-hover'>
@@ -20,7 +21,7 @@ const Index = ({ tHead, data, tableTitle }) => {
                                 <div className='row rounded bg-light text-dark align-items-center p-2'>
                                     {
                                         tHead.map((head, headIdx) => (
-                                            <div className='col text-center' key={headIdx}>{head}</div>
+                                            <div className='col text-center body_2 neutral_2' key={headIdx}>{head}</div>
                                         ))
                                     }
                                 </div>
@@ -32,20 +33,20 @@ const Index = ({ tHead, data, tableTitle }) => {
                                             <div className='row rounded bg-light text-dark align-items-center' key={itemIdx}>
                                                 <Accordion.Item eventKey={itemIdx}>
                                                     <Accordion.Header>
-                                                        <div className='col text-center'>{item.title}</div>
-                                                        <div className='col text-center'>{item.desc.length >= 70 ? `${item.desc.substring(0, 70)}...` : item.desc}</div>
-                                                        <div className='col text-center'>{item.course}</div>
-                                                        <div className='col text-center'>{item.employee}</div>
-                                                        <div className='col d-flex justify-content-end px-3'>
-                                                            <button className='btn btn-sm btn-outline-warning'>Edit</button>
+                                                        <div className='col text-center caption_1 secondary_2'>{item.title}</div>
+                                                        <div className='col text-center caption_1 secondary_2'>{item.desc.length >= 70 ? `${item.desc.substring(0, 70)}...` : item.desc}</div>
+                                                        <div className='col text-center caption_1 secondary_2'>{item.course}</div>
+                                                        <div className='col text-center caption_1 secondary_2'>{item.employee}</div>
+                                                        <div className='col d-flex justify-content-end mx-3'>
+                                                            <Button type={"btn-edit"} />
                                                         </div>
                                                     </Accordion.Header>
                                                     <Accordion.Body>
                                                         <div className='d-flex justify-content-end px-4'>
-                                                            <div className='border text-center px-2 rounded border-danger'>
-                                                                <span className='align-middle'>Delete this category, will remove all the items and the courses in this category</span>
+                                                            <div className='mx-2 card_alert'>
+                                                                <span className='alert_delete caption_1 secondary_2'>Delete this category, will remove all the items and the courses in this category</span>
                                                             </div>
-                                                            <button className='btn btn-sm btn-outline-danger mx-2'>Delete</button>
+                                                            <Button type={"btn-delete"} />
                                                         </div>
                                                     </Accordion.Body>
                                                 </Accordion.Item>
