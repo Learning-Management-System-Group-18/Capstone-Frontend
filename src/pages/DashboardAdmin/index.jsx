575f55fcfbd74b7919409c4986bc50f21aa7c598
Muhammad Rahman <rahman.muraman@gmail.com>
2022-06-20 23:43:19 +0700

    Edit styling DashboardAdmin

--- src/pages/DashboardAdmin/index.jsx
+++ src/pages/DashboardAdmin/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import {
   Card,
   Button,
@@ -6,24 +6,24 @@ import {
   NavbarAdmin,
   SearchBar,
   Notification,
-} from '../../components';
-import { employIcon, categoriesIcon, courseIcon } from '../../assets';
-import './style.css';
+} from "../../components";
+import { employIcon, categoriesIcon, courseIcon } from "../../assets";
+import "./style.css";
 
 const index = () => {
   const tHead = [
-    'Category Name',
-    'Description',
-    'Course',
-    'Employee',
-    'Action',
+    "Category Name",
+    "Description",
+    "Course",
+    "Employee",
+    "Action",
   ];
   const data = [
-    'Android Developer',
-    'Android developer adalah course yang mempelajari cara mengembangkan aplikasi mobile dengan bahasa Dar...',
-    '12 Course',
-    '121 Employee',
-    'Edit',
+    "Android Developer",
+    "Android developer adalah course yang mempelajari cara mengembangkan aplikasi mobile dengan bahasa Dar...",
+    "12 Course",
+    "121 Employee",
+    "Edit",
   ];
 
   return (
@@ -31,39 +31,32 @@ const index = () => {
       <NavbarAdmin />
       <div className="background">
         <div className="nav-info">
-          <div className="container d-flex justify-content-between p-3">
+          <div className="container d-flex justify-content-between py-3">
             <div className="nav-name">
               <span className="title-nav">Welcome back</span>
               <h5>Latifa Salsabila 👋</h5>
             </div>
 
-            <div className="d-flex gap-3">
-              <div className="search-item">
-                <SearchBar />
-              </div>
-              <div className="notification-item">
-                <Notification />
-              </div>
+            <div className="d-flex align-items-center gap-3">
+              <SearchBar />
+              <Notification />
             </div>
           </div>
         </div>
+
         <div className="container d-flex gap-4 mt-4">
-          <Card icon={categoriesIcon} total={7} desc={'Total of Categories'} />
-          <Card icon={courseIcon} total={7} desc={'Total of Course'} />
-          <Card icon={employIcon} total={7} desc={'Total of Employess'} />
+          <Card icon={categoriesIcon} total={7} desc={"Total of Categories"} />
+          <Card icon={courseIcon} total={7} desc={"Total of Course"} />
+          <Card icon={employIcon} total={7} desc={"Total of Employess"} />
         </div>
-        <div className="background-item">
-          <div className="container d-flex justify-content-between mt-5">
-            <div className="category-name">Category</div>
-            <div className="button-add">
-              {' '}
-              <Button type={'btn-add'} />
-            </div>
+
+        <div className="container background-table px-5 pb-5 mt-5">
+          <div className="d-flex justify-content-between align-items-end py-4">
+            <div className="category-name heading_4">Category</div>
+            <Button type={"btn-add"} />
           </div>
 
-          <div className="container mt-4">
-            <Table tHead={tHead} data={data} />
-          </div>
+          <Table tHead={tHead} data={data} />
         </div>
       </div>
     </>
