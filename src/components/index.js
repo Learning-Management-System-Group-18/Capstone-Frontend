c5673e481ef385dbc365c52b8eecd14ccce3c4bc
Muhammad Rahman <rahman.muraman@gmail.com>
2022-06-20 23:50:22 +0700

    add progressbar component to index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,17 @@
-import Button from './Button';
-import Card from './Card';
-import Table from './Table';
-import NavbarAdmin from './NavbarAdmin';
-import SearchBar from './SearchBar';
-import Notification from './Notification';
+import Button from "./Button";
+import Card from "./Card";
+import Table from "./Table";
+import NavbarAdmin from "./NavbarAdmin";
+import SearchBar from "./SearchBar";
+import Notification from "./Notification";
+import ProgressBar from "./ProggresBar";
 
-export { Button, Card, Table, NavbarAdmin, SearchBar, Notification };
+export {
+  Button,
+  Card,
+  Table,
+  NavbarAdmin,
+  SearchBar,
+  Notification,
+  ProgressBar,
+};
