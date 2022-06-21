dd8fea658aef59e050dab511e40b387d0dfa86c7
Learning-Management-System-Group-18 <yusufmaolana001@ummi.ac.id>
2022-06-21 20:39:01 +0700

    add new styling

--- src/pages/DashboardAdmin/index.jsx
+++ src/pages/DashboardAdmin/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 import {
   Card,
   Button,
@@ -6,24 +6,32 @@ import {
   NavbarAdmin,
   SearchBar,
   Notification,
-} from "../../components";
-import { employIcon, categoriesIcon, courseIcon } from "../../assets";
-import "./style.css";
+} from '../../components';
+import { employIcon, categoriesIcon, courseIcon } from '../../assets';
+import './style.css';
 
 const index = () => {
+  const tableTitle = ['Category'];
   const tHead = [
-    "Category Name",
-    "Description",
-    "Course",
-    "Employee",
-    "Action",
+    'Category Name',
+    'Description',
+    'Course',
+    'Employee',
+    'Action',
   ];
   const data = [
-    "Android Developer",
-    "Android developer adalah course yang mempelajari cara mengembangkan aplikasi mobile dengan bahasa Dar...",
-    "12 Course",
-    "121 Employee",
-    "Edit",
+    {
+      title: 'Android Developer',
+      desc: 'Android developer adalah course yang mempelajari cara mengembangkan aplikasi mobile dengan bahasa Dar...',
+      course: '12 Course',
+      employee: 'Coba',
+    },
+    {
+      title: 'HTML',
+      desc: 'Android developer adalah course yang mempelajari cara mengembangkan aplikasi mobile dengan bahasa Dar...',
+      course: '12 Course',
+      employee: 'Coba',
+    },
   ];
 
   return (
@@ -45,18 +53,17 @@ const index = () => {
         </div>
 
         <div className="container d-flex gap-4 mt-4">
-          <Card icon={categoriesIcon} total={7} desc={"Total of Categories"} />
-          <Card icon={courseIcon} total={7} desc={"Total of Course"} />
-          <Card icon={employIcon} total={7} desc={"Total of Employess"} />
+          <Card icon={categoriesIcon} total={7} desc={'Total of Categories'} />
+          <Card icon={courseIcon} total={7} desc={'Total of Course'} />
+          <Card icon={employIcon} total={7} desc={'Total of Employess'} />
         </div>
 
         <div className="container background-table px-5 pb-5 mt-5">
-          <div className="d-flex justify-content-between align-items-end py-4">
-            <div className="category-name heading_4">Category</div>
-            <Button type={"btn-add"} />
+          <div className="button">
+            <Button type={'btn-add'} />
           </div>
 
-          <Table tHead={tHead} data={data} />
+          <Table tHead={tHead} data={data} tableTitle={tableTitle} />
         </div>
       </div>
     </>
