c93555ad6b52a6377b8dabad2d837a0340f9ace2
Muhammad Rahman <rahman.muraman@gmail.com>
2022-06-22 23:10:57 +0700

    edit style profile

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -17,7 +17,7 @@ const Index = () => {
               <div className="heading_5">Account Setting</div>
             </div>
 
-            <div className="col-4 mt-4">
+            <div className="col-4 mt-3">
               <div className="wrap-user pb-3">
                 <div className="d-flex flex-column justify-content-center align-items-center py-3">
                   <div className="img-profile mb-5">
@@ -42,7 +42,7 @@ const Index = () => {
                 </div>
               </div>
             </div>
-            <div className="col-8 mt-4 ">
+            <div className="col-8 mt-3 ">
               <div className="wrap-data-user">
                 <div className="container">
                   <form className="row">
@@ -53,39 +53,42 @@ const Index = () => {
                       <label className="label-input-user neutral_5">
                         First Name
                       </label>
-                      <input type="text" className="form-control" />
+                      <input type="text" className="form-control radius-8" />
                     </div>
                     <div className="form-group col-6">
                       <label className="label-input-user neutral_5">
                         Last Name
                       </label>
-                      <input type="text" className="form-control" />
+                      <input type="text" className="form-control radius-8" />
                     </div>
 
                     <div className="form-group col-12 mt-4">
                       <label className="label-input-user neutral_5">
                         Email
                       </label>
-                      <input type="email" className="form-control" />
+                      <input type="email" className="form-control radius-8" />
                     </div>
                     <div className="form-group col-12 mt-4">
                       <label className="label-input-user neutral_5">
                         Phone Number
                       </label>
-                      <input type="number" className="form-control" />
+                      <input type="number" className="form-control radius-8" />
                     </div>
 
                     <div className="form-group col-6 mt-4">
                       <label className="label-input-user neutral_5">
                         Date of Birth
                       </label>
-                      <input type="date" className="form-control neutral_3" />
+                      <input
+                        type="date"
+                        className="form-control radius-8 neutral_3"
+                      />
                     </div>
                     <div className="form-group col-6 mt-4">
                       <label className="label-input-user neutral_5">
                         Gender
                       </label>
-                      <Form.Select className="neutral_3">
+                      <Form.Select className="neutral_3 radius-8">
                         <option value="Man">Man</option>
                         <option value="Woman">Woman</option>
                       </Form.Select>
@@ -98,35 +101,41 @@ const Index = () => {
                       <label className="label-input-user neutral_5">
                         Employee ID
                       </label>
-                      <input type="number" className="form-control" />
+                      <input type="number" className="form-control radius-8" />
                     </div>
                     <div className="form-group col-6">
                       <label className="label-input-user neutral_5">Role</label>
-                      <input type="text" className="form-control" />
+                      <input type="text" className="form-control radius-8" />
                     </div>
 
                     <div className="form-group col-6 mt-4">
                       <label className="label-input-user neutral_5">
                         Status
                       </label>
-                      <input type="text" className="form-control" />
+                      <input type="text" className="form-control radius-8" />
                     </div>
 
                     <div className="form-group col-6 mt-4">
                       <label className="label-input-user neutral_5">
                         Hire Date
                       </label>
-                      <input type="date" className="form-control neutral_3" />
+                      <input
+                        type="date"
+                        className="form-control radius-8 neutral_3"
+                      />
                     </div>
 
                     <div className="form-group col-12 mt-4">
                       <label className="label-input-user neutral_5">
                         Address
                       </label>
-                      <input type="text" className="form-control" />
+                      <input type="text" className="form-control radius-8" />
                     </div>
 
-                    <Button className="col-8 offset-2 bg_primary subtitle_2 mt-5 mb-4 py-3 px-4 radius-12">
+                    <Button
+                      className="col-8 offset-2 bg_primary subtitle_2 mt-5 mb-4 py-3 px-4 radius-12"
+                      disabled={true}
+                    >
                       Save Changes
                     </Button>
                   </form>
