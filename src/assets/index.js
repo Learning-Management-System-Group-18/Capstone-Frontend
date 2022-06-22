19c028df8c3d631c8a4b530a3e942c99bef4d270
mumtazdedi <mumtazdedi@gmail.com>
2022-06-22 21:11:30 +0700

    first commit page allcourse

--- src/assets/index.js
+++ src/assets/index.js
@@ -4,5 +4,6 @@ import Ellipse2 from './img/Ellipse 20.png';
 import employIcon from './img/Frame 366.png';
 import courseIcon from './img/Frame 366(1).png';
 import categoriesIcon from './img/Frame 366(2).png';
+import arrowRightIcon from './img/Arrow_right.svg';
 
-export { Rafiki, Ellipse, Ellipse2, employIcon, courseIcon, categoriesIcon };
+export { Rafiki, Ellipse, Ellipse2, employIcon, courseIcon, categoriesIcon, arrowRightIcon };
---
+++ src/pages/Course/index.jsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import {
+    Card,
+    Button,
+    Table,
+    NavbarAdmin,
+    SearchBar,
+    Notification,
+} from '../../components';
+import { employIcon, categoriesIcon, courseIcon, arrowRightIcon } from '../../assets';
+import './style.css';
+
+const index = () => {
+    const tableTitle = ['Category'];
+    const tHead = [
+        'Category Name',
+        'Description',
+        'Course',
+        'Employee',
+        'Action',
+    ];
+    const data = [
+        {
+            title: 'Android Developer',
+            desc: 'Android developer adalah course yang mempelajari cara mengembangkan aplikasi mobile dengan bahasa Dar...',
+            course: '12 Course',
+            employee: 'Coba',
+            icon: 'https://img.icons8.com/ultraviolet/344/play-button-circled.png',
+        },
+        {
+            title: 'HTML',
+            desc: 'Android developer adalah course yang mempelajari cara mengembangkan aplikasi mobile dengan bahasa Dar...',
+            course: '12 Course',
+            employee: 'Coba',
+            icon: 'https://img.icons8.com/color/344/apple-app-store--v1.png',
+        },
+    ];
+
+    return (
+        <>
+            <NavbarAdmin />
+            <div className="background">
+                <div className="nav-info">
+                    <div className="container d-flex justify-content-between py-3">
+                        <div className="nav-name">
+                            <span className='align-middle caption_2'>Dashboard <img src={arrowRightIcon} alt="arrow-right" /> Marketing </span>
+                        </div>
+
+                        <div className="d-flex align-middle align-items-center gap-3">
+                            <SearchBar />
+                            <Notification />
+                        </div>
+                    </div>
+                </div>
+
+                <div className="container d-flex gap-4 mt-4">
+                    <Card icon={categoriesIcon} total={7} desc={'Total of Categories'} />
+                    <Card icon={courseIcon} total={7} desc={'Total of Course'} />
+                    <Card icon={employIcon} total={7} desc={'Total of Employess'} />
+                </div>
+
+                <div className="container background-table px-5 pb-5 mt-5">
+
+                    <Table tHead={tHead} data={data} tableTitle={tableTitle} />
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default index;
