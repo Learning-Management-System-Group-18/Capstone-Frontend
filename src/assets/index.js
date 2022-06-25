5c3104bb3e94131957d1c136330054e10ab7393a
Learning-Management-System-Group-18 <yusufmaolana001@ummi.ac.id>
2022-06-25 17:03:41 +0700

    Change assest index

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,23 +1,25 @@
-import Rafiki from "./img/rafiki.png";
-import Ellipse from "./img/Ellipse 19.png";
-import Ellipse2 from "./img/Ellipse 20.png";
-import employIcon from "./img/Frame 366.png";
-import courseIcon from "./img/Frame 366(1).png";
-import categoriesIcon from "./img/Frame 366(2).png";
-import ProfileImg from "./img/profile.jpg";
+import Rafiki from './img/rafiki.png';
+import Ellipse from './img/Ellipse 19.png';
+import Ellipse2 from './img/Ellipse 20.png';
+import employIcon from './img/Frame 366.png';
+import courseIcon from './img/Frame 366(1).png';
+import categoriesIcon from './img/Frame 366(2).png';
+import ProfileImg from './img/profile.jpg';
 import deletepopupIcon from './img/Group 115.png';
 import totalMentorIcon from './img/total_mentor_icon.png';
 import arrowRightIcon from './img/Arrow_right.svg';
+import logoutIcon from './img/FrameLogout.png';
 
 export {
-    Rafiki,
-    Ellipse,
-    Ellipse2,
-    employIcon,
-    courseIcon,
-    categoriesIcon,
-    deletepopupIcon,
-    ProfileImg,
-    totalMentorIcon,
-    arrowRightIcon
+  Rafiki,
+  Ellipse,
+  Ellipse2,
+  employIcon,
+  courseIcon,
+  categoriesIcon,
+  deletepopupIcon,
+  ProfileImg,
+  totalMentorIcon,
+  arrowRightIcon,
+  logoutIcon,
 };
