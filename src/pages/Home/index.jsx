c426af25422353274db2cd530b099a2d9db11c6e
Kevin Jeremia Soaduon Pasaribu <64350998+LilKeJer@users.noreply.github.com>
2022-07-08 00:22:57 +0700

    Update Component Slicing Landing Page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,19 +1,265 @@
-
 import React from "react";
 import { useNavigate, Link } from "react-router-dom";
 import { Row, Col } from "react-bootstrap";
-import { FooterUser } from "../../components";
+import {
+  FooterUser,
+  CardReview,
+  ClassCategory,
+  PopularClassCard,
+} from "../../components";
 import NavBarLandingPage from "./NavBarLandingPage";
 import SearchLanding from "./searchLanding";
 import FAQ from "./FrequentlyAskQuestion";
 import "./style.css";
 import { LandingPagePhoto } from "../../assets";
 
-
 function Index() {
   const navigate = useNavigate();
-  return (
+  const myClassData = [
+    {
+      status: "ongoing",
+      imgCourse:
+        "https://images.unsplash.com/photo-1503428593586-e225b39bddfe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+      titleCourse: "Social Media Marketing",
+      categoryCourse: "Business Development",
+      description: "Description...",
+      totalSection: 156,
+      isCompleted: 70,
+      level: "Intermediate",
+    },
+    {
+      status: "ongoing",
+      imgCourse:
+        "https://images.unsplash.com/photo-1542744173-8e7e53415bb0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+      titleCourse: "Social Media Marketing",
+      categoryCourse: "Business Development",
+      description: "Description...",
+      totalSection: 156,
+      isCompleted: 70,
+      level: "Intermediate",
+    },
+  ];
 
+  const classCategoryData = [
+    {
+      title: "Business Development",
+      icon: "https://icon-library.com/images/android-icon/android-icon-5.jpg",
+    },
+    {
+      title: "Android Development",
+      icon: "https://icon-library.com/images/android-icon/android-icon-5.jpg",
+    },
+    {
+      title: "Web Development",
+      icon: "https://icon-library.com/images/android-icon/android-icon-5.jpg",
+    },
+    {
+      title: "Business Development",
+      icon: "https://icon-library.com/images/android-icon/android-icon-5.jpg",
+    },
+    {
+      title: "Android Development",
+      icon: "https://icon-library.com/images/android-icon/android-icon-5.jpg",
+    },
+    {
+      title: "Web Development",
+      icon: "https://icon-library.com/images/android-icon/android-icon-5.jpg",
+    },
+    {
+      title: "Web Development",
+      icon: "https://icon-library.com/images/android-icon/android-icon-5.jpg",
+    },
+  ];
+
+  const popularClassData = [
+    {
+      class_category: "Business Development",
+      img: "https://images.unsplash.com/photo-1503428593586-e225b39bddfe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+      title: "Social Media Marketing",
+      price: 0,
+      level: "Intermediate",
+      total_employee: 120,
+      star: 3,
+      total_review: 110,
+    },
+    {
+      class_category: "Android Development",
+      img: "https://images.unsplash.com/photo-1503428593586-e225b39bddfe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+      title: "Android Dev",
+      price: 0,
+      level: "Beginner",
+      total_employee: 150,
+      star: 4,
+      total_review: 120,
+    },
+    {
+      class_category: "Business Development",
+      img: "https://images.unsplash.com/photo-1503428593586-e225b39bddfe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+      title: "Social Media Marketing",
+      price: 0,
+      level: "Advanced",
+      total_employee: 120,
+      star: 3,
+      total_review: 110,
+    },
+    {
+      class_category: "Android Development",
+      img: "https://images.unsplash.com/photo-1503428593586-e225b39bddfe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+      title: "Android Dev",
+      price: 0,
+      level: "Beginner",
+      total_employee: 150,
+      star: 4,
+      total_review: 120,
+    },
+    {
+      class_category: "Business Development",
+      img: "https://images.unsplash.com/photo-1503428593586-e225b39bddfe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+      title: "Social Media Marketing",
+      price: 0,
+      level: "Intermediate",
+      total_employee: 120,
+      star: 3,
+      total_review: 110,
+    },
+    {
+      class_category: "Android Development",
+      img: "https://images.unsplash.com/photo-1503428593586-e225b39bddfe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+      title: "Android Dev",
+      price: 0,
+      level: "Beginner",
+      total_employee: 150,
+      star: 4,
+      total_review: 120,
+    },
+    {
+      class_category: "Business Development",
+      img: "https://images.unsplash.com/photo-1503428593586-e225b39bddfe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+      title: "Social Media Marketing",
+      price: 0,
+      level: "Intermediate",
+      total_employee: 120,
+      star: 3,
+      total_review: 110,
+    },
+    {
+      class_category: "Android Development",
+      img: "https://images.unsplash.com/photo-1503428593586-e225b39bddfe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+      title: "Android Dev",
+      price: 0,
+      level: "Beginner",
+      total_employee: 150,
+      star: 4,
+      total_review: 120,
+    },
+    {
+      class_category: "Android Development",
+      img: "https://images.unsplash.com/photo-1503428593586-e225b39bddfe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+      title: "Android Dev",
+      price: 0,
+      level: "Beginner",
+      total_employee: 150,
+      star: 4,
+      total_review: 120,
+    },
+  ];
+
+  const mentorData = [
+    {
+      mentor_name: "John Doe",
+      img: "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+      bio: "Head of office",
+    },
+    {
+      mentor_name: "Linka Angel",
+      img: "https://images.unsplash.com/photo-1504593811423-6dd665756598?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+      bio: "Head of Developer",
+    },
+    {
+      mentor_name: "Prisce",
+      img: "https://images.unsplash.com/photo-1503428593586-e225b39bddfe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+      bio: "Head of Marketing",
+    },
+    {
+      mentor_name: "John Doe",
+      img: "https://images.unsplash.com/photo-1503428593586-e225b39bddfe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+      bio: "Head of office",
+    },
+    {
+      mentor_name: "Linka Angel",
+      img: "https://images.unsplash.com/photo-1503428593586-e225b39bddfe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+      bio: "Head of Developer",
+    },
+    {
+      mentor_name: "Prisce",
+      img: "https://images.unsplash.com/photo-1503428593586-e225b39bddfe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+      bio: "Head of Marketing",
+    },
+    {
+      mentor_name: "John Doe",
+      img: "https://images.unsplash.com/photo-1503428593586-e225b39bddfe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+      bio: "Head of office",
+    },
+    {
+      mentor_name: "Linka Angel",
+      img: "https://images.unsplash.com/photo-1503428593586-e225b39bddfe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+      bio: "Head of Developer",
+    },
+    {
+      mentor_name: "Prisce",
+      img: "https://images.unsplash.com/photo-1503428593586-e225b39bddfe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+      bio: "Head of Marketing",
+    },
+  ];
+
+  const reviewData = [
+    {
+      quotes_desc:
+        "Online course yang paling keren sih, materi yang disampaikan benar benar yang dipakai di dunia kerja dan dapat portfolio yang bisa digunakan untuk melamar magang atau kerja",
+      reviewer_profile: {
+        img_reviewer: "https://i.pravatar.cc/300",
+        reviewer_name: "ani",
+        reviewer_job: "Frontend Developer",
+      },
+    },
+    {
+      quotes_desc:
+        "Online course yang paling keren sih, materi yang disampaikan benar benar yang dipakai di dunia kerja dan dapat portfolio yang bisa digunakan untuk melamar magang atau kerja",
+      reviewer_profile: {
+        img_reviewer: "https://i.pravatar.cc/400",
+        reviewer_name: "Rizky",
+        reviewer_job: "Backend Developer",
+      },
+    },
+    {
+      quotes_desc:
+        "Online course yang paling keren sih, materi yang disampaikan benar benar yang dipakai di dunia kerja dan dapat portfolio yang bisa digunakan untuk melamar magang atau kerja",
+      reviewer_profile: {
+        img_reviewer: "https://i.pravatar.cc/500",
+        reviewer_name: "Nopal",
+        reviewer_job: "Backend Developer",
+      },
+    },
+    {
+      quotes_desc:
+        "Online course yang paling keren sih, materi yang disampaikan benar benar yang dipakai di dunia kerja dan dapat portfolio yang bisa digunakan untuk melamar magang atau kerja",
+      reviewer_profile: {
+        img_reviewer: "https://i.pravatar.cc/400",
+        reviewer_name: "Rizky",
+        reviewer_job: "Backend Developer",
+      },
+    },
+    {
+      quotes_desc:
+        "Online course yang paling keren sih, materi yang disampaikan benar benar yang dipakai di dunia kerja dan dapat portfolio yang bisa digunakan untuk melamar magang atau kerja",
+      reviewer_profile: {
+        img_reviewer: "https://i.pravatar.cc/500",
+        reviewer_name: "Nopal",
+        reviewer_job: "Backend Developer",
+      },
+    },
+  ];
+  return (
     <div>
       <NavBarLandingPage />
       <div className="container">
@@ -37,7 +283,11 @@ function Index() {
           </Col>
         </Row>
       </div>
+      <ClassCategory title={"Class Category"} cardData={classCategoryData} />
+      <PopularClassCard data={popularClassData} />
+
       <FAQ />
+      <CardReview data={reviewData} />
       <div className="started">
         <div style={{ padding: "58px 87.5px 58px 87.5px" }} className="d-flex ">
           <div>
@@ -66,7 +316,6 @@ function Index() {
         </div>
       </div>
       <FooterUser />
-
     </div>
   );
 }
