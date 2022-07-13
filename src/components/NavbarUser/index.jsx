726917782b0c3b72b4dd891be637609d540c7ff2
Learning-Management-System-Group-18 <yusufmaolana001@ummi.ac.id>
2022-07-14 05:47:48 +0700

    Edit Navbar User

--- src/components/NavbarUser/index.jsx
+++ src/components/NavbarUser/index.jsx
@@ -1,14 +1,14 @@
-import { React, useState } from "react";
-import "./style.css";
-import { Dropdown, NavDropdown, Nav, Navbar, Container } from "react-bootstrap";
-import { Link, useLocation } from "react-router-dom";
-import { FiSearch } from "react-icons/fi";
-import { navUserIcon, profileUserIcon } from "../../assets";
+import { React, useState } from 'react';
+import './style.css';
+import { Dropdown, NavDropdown, Nav, Navbar, Container } from 'react-bootstrap';
+import { Link, useLocation } from 'react-router-dom';
+import { FiSearch } from 'react-icons/fi';
+import { navUserIcon, profileUserIcon } from '../../assets';
 
 const Index = () => {
   const location = useLocation();
   const locationLink = location.pathname;
-  const link = locationLink.split("/");
+  const link = locationLink.split('/');
 
   // const [showLogout, setShowLogout] = useState(false);
 
@@ -19,17 +19,17 @@ const Index = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark" id="nav-user">
       <div className="container ms-4">
-        <Link to="/home">
+        <Link to="/">
           <img src={navUserIcon} alt="navUserIcon" className="icon-navuser" />
         </Link>
         <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
           <Nav className="mx-auto">
             <Nav>
               <Link
-                to="/"
+                to="/home"
                 className={`${
-                  link[1] == "home"
-                    ? `item-nav-user decorationuser`
+                  link[1] == 'home'
+                    ? `item-nav-user decoration-Navuser `
                     : `item-nav-user`
                 }`}
               >
@@ -38,10 +38,10 @@ const Index = () => {
             </Nav>
             <Nav>
               <Link
-                to="/mycourse"
+                to="/my-class"
                 className={`${
-                  link[1] == "myclass"
-                    ? `item-nav-user decorationuser`
+                  link[1] == 'my-class'
+                    ? `item-nav-user decoration-Navuser `
                     : `item-nav-user`
                 }`}
               >
@@ -53,33 +53,50 @@ const Index = () => {
                 title="Category"
                 id="basic-nav-dropdown"
                 className={`${
-                  link[1] == "profile"
-                    ? `drop-nav-user decorationuser`
+                  link[1] == 'profile'
+                    ? `drop-nav-user decoration-Navuser`
                     : `drop-nav-user`
                 }`}
-                style={{ color: "black" }}
+                style={{ color: 'black' }}
               >
                 <NavDropdown.Item className="drop-item-nav-user">
-                  <Link to="/none">None</Link>
+                  <Link to="/none">Web & Mobile Developer</Link>
                 </NavDropdown.Item>
                 <NavDropdown.Item className="drop-item-nav-user">
-                  <Link to="/none">None</Link>
+                  <Link to="/none">UI/UX Designer</Link>
+                </NavDropdown.Item>
+                <NavDropdown.Item className="drop-item-nav-user">
+                  <Link to="/none">Business Development</Link>
+                </NavDropdown.Item>
+                <NavDropdown.Item className="drop-item-nav-user">
+                  <Link to="/none">Finance & Accounting</Link>
+                </NavDropdown.Item>
+                <NavDropdown.Item className="drop-item-nav-user">
+                  <Link to="/none">Data Analyst</Link>
+                </NavDropdown.Item>
+                <NavDropdown.Item className="drop-item-nav-user gap">
+                  <Link to="/none">Marketing</Link>
+                </NavDropdown.Item>
+                <NavDropdown.Item className="drop-item-nav-user">
+                  <Link to="/none">Career Preparation</Link>
                 </NavDropdown.Item>
               </NavDropdown>
             </Nav>
           </Nav>
         </div>
       </div>
-      <div className="searchNavUser">
-        <FiSearch className="icon-searchusernav" />
+      <div className="navuser-Search">
+        <FiSearch className="navuser-SearchIcon" />
         <input
           type="text"
           placeholder="Social media specialist"
-          className="input-searchnav-user"
+          className="navuser-SearchInput"
         ></input>
       </div>
       <div className="iconProfileUser">
-        <img src={profileUserIcon} className="img-usernav" alt="icon" />
+        <Link to="/user-profile">
+          <img src={profileUserIcon} className="img-usernav" alt="icon" />
+        </Link>
       </div>
     </nav>
   );
