c873b3616281483de12647ff5e63c53fca6b3269
Muhammad Rahman <rahman.muraman@gmail.com>
2022-07-14 10:40:39 +0700

    add list for category

--- src/components/NavbarUser/index.jsx
+++ src/components/NavbarUser/index.jsx
@@ -1,21 +1,45 @@
-import { React, useState } from 'react';
-import './style.css';
-import { Dropdown, NavDropdown, Nav, Navbar, Container } from 'react-bootstrap';
-import { Link, useLocation } from 'react-router-dom';
-import { FiSearch } from 'react-icons/fi';
-import { navUserIcon, profileUserIcon } from '../../assets';
+import { React, useState } from "react";
+import "./style.css";
+import { NavDropdown, Nav } from "react-bootstrap";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { FiSearch } from "react-icons/fi";
+import { navUserIcon, profileUserIcon } from "../../assets";
+import { useEffect } from "react";
+import axiosInstance from "../../networks/apis";
 
 const Index = () => {
+  const navigate = useNavigate();
   const location = useLocation();
   const locationLink = location.pathname;
-  const link = locationLink.split('/');
+  const link = locationLink.split("/");
 
-  // const [showLogout, setShowLogout] = useState(false);
+  const [categories, setCategories] = useState([]);
 
-  // const handleCloseLogout = () => setShowLogout(false);
-  // const logoutShow = () => {
-  //   setShowLogout(true);
-  // };
+  useEffect(async () => {
+    await axiosInstance
+      .get("api/categories")
+      .then((response) => {
+        // console.log(response.data.data);
+        setCategories(response.data.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  const handleCategoryClass = (id, name) => {
+    let titleSlug = name
+      .replace(/[`~!@#$%^&*()_\-+=\[\]{};:'"\\|\/,.<>?\s]/g, " ")
+      .toLowerCase();
+    titleSlug = titleSlug.replace(/^\s+|\s+$/gm, "");
+    titleSlug = titleSlug.replace(/\s+/g, "-");
+
+    console.log(titleSlug);
+    navigate(`/class-category/${titleSlug}/${id}`);
+  };
+
+  console.log(categories);
+
   return (
     <nav className="navbar navbar-expand-lg navbar-dark" id="nav-user">
       <div className="container ms-4">
@@ -28,7 +52,7 @@ const Index = () => {
               <Link
                 to="/home"
                 className={`${
-                  link[1] == 'home'
+                  link[1] == "home"
                     ? `item-nav-user decoration-Navuser `
                     : `item-nav-user`
                 }`}
@@ -40,7 +64,7 @@ const Index = () => {
               <Link
                 to="/my-class"
                 className={`${
-                  link[1] == 'my-class'
+                  link[1] == "my-class"
                     ? `item-nav-user decoration-Navuser `
                     : `item-nav-user`
                 }`}
@@ -53,33 +77,23 @@ const Index = () => {
                 title="Category"
                 id="basic-nav-dropdown"
                 className={`${
-                  link[1] == 'profile'
+                  link[1] == "profile"
                     ? `drop-nav-user decoration-Navuser`
                     : `drop-nav-user`
                 }`}
-                style={{ color: 'black' }}
+                style={{ color: "black" }}
               >
-                <NavDropdown.Item className="drop-item-nav-user">
-                  <Link to="/none">Web & Mobile Developer</Link>
-                </NavDropdown.Item>
-                <NavDropdown.Item className="drop-item-nav-user">
-                  <Link to="/none">UI/UX Designer</Link>
-                </NavDropdown.Item>
-                <NavDropdown.Item className="drop-item-nav-user">
-                  <Link to="/none">Business Development</Link>
-                </NavDropdown.Item>
-                <NavDropdown.Item className="drop-item-nav-user">
-                  <Link to="/none">Finance & Accounting</Link>
-                </NavDropdown.Item>
-                <NavDropdown.Item className="drop-item-nav-user">
-                  <Link to="/none">Data Analyst</Link>
-                </NavDropdown.Item>
-                <NavDropdown.Item className="drop-item-nav-user gap">
-                  <Link to="/none">Marketing</Link>
-                </NavDropdown.Item>
-                <NavDropdown.Item className="drop-item-nav-user">
-                  <Link to="/none">Career Preparation</Link>
-                </NavDropdown.Item>
+                {categories?.map((category, i) => (
+                  <NavDropdown.Item
+                    className="drop-item-nav-user"
+                    key={i}
+                    onClick={() =>
+                      handleCategoryClass(category.id, category.title)
+                    }
+                  >
+                    {category.title}
+                  </NavDropdown.Item>
+                ))}
               </NavDropdown>
             </Nav>
           </Nav>
