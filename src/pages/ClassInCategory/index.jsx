c10de29cd193f4697bceff078cbfaf377f56b870
Muhammad Rahman <rahman.muraman@gmail.com>
2022-07-14 10:40:13 +0700

    filter data by category

--- src/pages/ClassInCategory/index.jsx
+++ src/pages/ClassInCategory/index.jsx
@@ -1,19 +1,24 @@
-import React from 'react';
-import { NavbarUser, FooterUser, PopularClassCard } from '../../components';
-import Dropdown from 'react-bootstrap/Dropdown';
-import DropdownButton from 'react-bootstrap/DropdownButton';
-import { useEffect, useState } from 'react';
-import axiosInstance from '../../networks/apis';
-import './style.css';
-
+import React from "react";
+import { NavbarUser, FooterUser, PopularClassCard } from "../../components";
+import Dropdown from "react-bootstrap/Dropdown";
+import DropdownButton from "react-bootstrap/DropdownButton";
+import { useEffect, useState } from "react";
+import axiosInstance from "../../networks/apis";
+import { noData } from "../../assets";
+import "./style.css";
+import { useParams } from "react-router-dom";
 const Index = () => {
+  const { categoryName, idCategory } = useParams();
+
+  console.log(typeof idCategory);
+
   const [popularCourse, setPopularCourse] = useState([]);
-  useEffect(() => {
-    axiosInstance
-      .get('/api/course/popular')
+  useEffect(async () => {
+    await axiosInstance
+      .get("/api/course/popular")
       .then((response) => {
-        console.log(response.data.data);
-        setPopularCourse(...popularCourse, response.data.data);
+        // console.log(response.data.data);
+        setPopularCourse(response.data.data);
         // console.log(popularCourse);
       })
       .catch((error) => {
@@ -21,12 +26,31 @@ const Index = () => {
       });
   }, []);
 
+  const newData = popularCourse.filter((x) => {
+    if (x.category.id === Number(idCategory)) {
+      return x;
+      console.log("first");
+    } else {
+      // return x;
+      console.log(x.category.id);
+    }
+  });
+
+  console.log("dataFilter ", newData);
+
+  // console.log(popularCourse);
+
   return (
     <>
       <NavbarUser />
 
       <div className="container d-flex justify-content-between py-5 px-4">
-        <div className="titleClassCategory"> Marketing </div>
+        <div
+          className="titleClassCategory"
+          style={{ textTransform: "capitalize" }}
+        >
+          {categoryName.replaceAll("-", " ")}
+        </div>
         <DropdownButton
           id="dropdown-basic-button"
           title="Filter by level"
@@ -44,7 +68,17 @@ const Index = () => {
         </DropdownButton>
       </div>
 
-      <PopularClassCard data={popularCourse} />
+      {newData?.length > 0 ? (
+        <PopularClassCard data={newData || []} />
+      ) : (
+        <div className="container text-center my-5 ">
+          <img src={noData} alt="no-data" />
+          <div className="body_1 mt-3">
+            Data Kategori {categoryName.replaceAll("-", " ")} Masih Belum
+            Tersedia{" "}
+          </div>
+        </div>
+      )}
 
       <footer>
         <FooterUser />
